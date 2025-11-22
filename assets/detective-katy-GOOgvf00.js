016f8112bcd9587d0cf820fba1a4ffe5a14b6e97
xavier <liangzhewei@gmail.com>
2025-11-21 23:10:10 -0700

    Updates

---
+++ assets/detective-katy-GOOgvf00.js
@@ -0,0 +1,12 @@
+import{R as t,j as e,a as i,S as a}from"./StoryPlayer-BDq9-VYH.js";const o=[{id:0,image:"1.webp",text:"凯蒂侦探和闪亮牙齿的秘密",subText:"保护牙齿，是我自己的任务！",type:"cover"},{id:1,image:"1.webp",text:`在小镇的阁楼上，住着一只最聪明的小猫，她的名字叫凯蒂。
+凯蒂不是普通的小猫，她是一位了不起的小侦探！
+每天，她都有一个非常、非常重要的案子要调查。`,type:"story"},{id:2,image:"2.webp",text:`今天早上，凯蒂吃完了甜甜的草莓和牛奶。她舔了舔嘴巴，忽然，她发现了一个神秘线索！
+“嗯？”凯蒂对着镜子里的自己说，“我的牙齿感觉毛茸茸的，一点也不闪亮了！”`,type:"story"},{id:3,image:"3.webp",text:"“我知道了！”凯蒂的眼睛一下子亮了起来，“一定是那些‘淘气糖虫’又来捣乱了！它们最喜欢躲在牙齿中间捉迷藏，让牙齿失去光芒。”",type:"story"},{id:4,image:"4.webp",text:`作为一名优秀的小侦探，凯蒂有她的秘密武器！
+她跑到自己的小浴室，拿起了她的“魔法泡泡刷”，挤上了亮晶晶的“薄荷味力量膏”。
+“好了，淘气糖虫们，我来啦！”`,type:"story"},{id:5,image:"5.webp",text:`凯蒂侦探开始行动了！
+她的魔法泡泡刷“唰唰唰”，在牙齿上面画着小圈圈，像在寻找糖虫留下的脚印。
+她唱着歌：“上面的牙齿刷一刷，下面的牙齿刷一刷，里面的牙齿也不能忘！”`,type:"story"},{id:6,image:"6.webp",text:`“找到你们啦！”凯蒂咕噜咕噜漱了漱口，噗——
+所有的淘气糖虫都坐着“水滑梯”，被冲走啦！`,type:"story"},{id:7,image:"7.webp",text:`凯蒂侦探再次看向镜子。
+哇！所有的牙齿都变得闪闪发光，像夜空中最亮的星星！
+她的笑容，是全世界最灿烂的笑容。`,type:"story"},{id:8,image:"8.webp",text:`案子解决了！
+凯蒂侦探骄傲地对自己说：“保护牙齿，是我自己的任务！明天早上，调查继续！”`,type:"end"}];t.createRoot(document.getElementById("root")).render(e.jsx(i.StrictMode,{children:e.jsx(a,{storyData:o})}));
